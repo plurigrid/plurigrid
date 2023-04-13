829ef49ea0c311f43433888e15aea62fc59e46cc
bluenote <janitachalam@gmail.com>
2023-04-12 18:03:30 -0700

    misguided baby AGI with chroma

--- plurigrid/agent/agents/baby_agi.py
+++ plurigrid/agent/agents/baby_agi.py
@@ -13,14 +13,14 @@ from langchain.vectorstores import faiss
 from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
 from langchain import OpenAI, SerpAPIWrapper, LLMChain
 from langchain.docstore import InMemoryDocstore
+from llama_index import GPTChromaIndex, SimpleDirectoryReader
+
 
 # todo: replace with chroma
 # Define your embedding model
-embeddings_model = OpenAIEmbeddings()
-# Initialize the vectorstore as empty
-embedding_size = 1536
-index = faiss.IndexFlatL2(embedding_size)
-vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+embeddings = OpenAIEmbeddings()
+texts = ["Figure out how a lot of people could come together around an energy cooperative movement and an AI-agency-game-theory-driven DAO structure with value-aligned incentives"]
+vectorstore = Chroma.from_texts(texts, embeddings, metadatas=[{"task": "Manage microgrid"}])
 
 class TaskCreationChain(LLMChain):
     """Chain to generates tasks."""
@@ -94,6 +94,44 @@ prompt = ZeroShotAgent.create_prompt(
     suffix=suffix,
     input_variables=["objective", "task", "context","agent_scratchpad"]
 )
+
+def get_next_task(task_creation_chain: LLMChain, result: Dict, task_description: str, task_list: List[str], objective: str) -> List[Dict]:
+    """Get the next task."""
+    incomplete_tasks = ", ".join(task_list)
+    response = task_creation_chain.run(result=result, task_description=task_description, incomplete_tasks=incomplete_tasks, objective=objective)
+    new_tasks = response.split('\n')
+    return [{"task_name": task_name} for task_name in new_tasks if task_name.strip()]
+
+
+def prioritize_tasks(task_prioritization_chain: LLMChain, this_task_id: int, task_list: List[Dict], objective: str) -> List[Dict]:
+    """Prioritize tasks."""
+    task_names = [t["task_name"] for t in task_list]
+    next_task_id = int(this_task_id) + 1
+    response = task_prioritization_chain.run(task_names=task_names, next_task_id=next_task_id, objective=objective)
+    new_tasks = response.split('\n')
+    prioritized_task_list = []
+    for task_string in new_tasks:
+        if not task_string.strip():
+            continue
+        task_parts = task_string.strip().split(".", 1)
+        if len(task_parts) == 2:
+            task_id = task_parts[0].strip()
+            task_name = task_parts[1].strip()
+            prioritized_task_list.append({"task_id": task_id, "task_name": task_name})
+    return prioritized_task_list
+
+def _get_top_tasks(vectorstore, query: str, k: int) -> List[str]:
+    """Get the top k tasks based on the query."""
+    results = vectorstore.similarity_search_with_score(query, k=k)
+    if not results:
+        return []
+    sorted_results, _ = zip(*sorted(results, key=lambda x: x[1], reverse=True))
+    return [str(item.metadata['task']) for item in sorted_results]
+
+def execute_task(vectorstore, execution_chain: LLMChain, objective: str, task: str, k: int = 1) -> str:
+    """Execute a task."""
+    context = _get_top_tasks(vectorstore, query=objective, k=k)
+    return execution_chain.run(objective=objective, context=context, task=task)
 
 class BabyAGI(Chain, BaseModel):
     """Controller model for the BabyAGI agent."""
@@ -212,8 +250,6 @@ class BabyAGI(Chain, BaseModel):
 OBJECTIVE = "Figure out how a lot of people could come together around an energy cooperative movement and an AI-agency-game-theory-driven DAO structure with value-aligned incentives to invest in a sustainable transition to a less centralized energy system owned, truly, by the people, where UBE (universal basic energy) is guaranteed and control by any single party is limited. Explain by telling the story of a Xeno-California which starts in 2023, and show how it evolves from 2023 to 2030 and to 2040. Make sure to think about recycling, seasonal excess energy use for gigaprojects such as desalination, de-desertification, long-term gravitational storage, space launches, etc. Make sure to also explore the interactions between small, local micrograms like a 30 house village, larger microgrids like a 10000 person town, and city grid projects. Also include energy and compute trading, AI edge nodes which can take sensor data, communicate with other nodes in a mesh, throttle energy, alert humans and AI agents to emergencies or pressing recommendations."
 
 
-
-
 llm = OpenAI(temperature=0)
 # Logging of LLMChains
 verbose=False
@@ -225,3 +261,5 @@ baby_agi = BabyAGI.from_llm(
     verbose=verbose,
     max_iterations=max_iterations
 )
+
+baby_agi({"objective": OBJECTIVE})
