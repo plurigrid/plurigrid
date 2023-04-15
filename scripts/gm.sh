d56aed0c6b5b372b4621fd8828ebde9fbefe3a66
bluenote <janitachalam@gmail.com>
2023-04-14 20:12:23 -0700

    gm script workss

--- scripts/gm.sh
+++ scripts/gm.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Set your GitHub repository URL and Docker image name here
-GITHUB_REPO_URL="https://github.com/janitaplurigrid/plurigrid"
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title gm
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ☀️
+
+# Documentation:
+# @raycast.description gm!
+# @raycast.author Plurigrid
+
 DOCKER_IMAGE_NAME="plurigrid:latest"
 
-# Clone the GitHub repository
-git clone "$GITHUB_REPO_URL"
-cd plurigrid
-
 # Pull the Docker image from the GitHub Container Registry
-docker pull ghcr.io/plurigrid/"$DOCKER_IMAGE_NAME"
+# docker pull ghcr.io/plurigrid/"$DOCKER_IMAGE_NAME"
 
 # Run the Docker image
-docker run -it --rm -v "${DOCKER_IMAGE_NAME}"
+# docker run -it --rm -v "${DOCKER_IMAGE_NAME}"
+
+osascript -e 'tell app "Terminal" to do script "docker run -it --platform linux/amd64 plurigrid:latest"'
