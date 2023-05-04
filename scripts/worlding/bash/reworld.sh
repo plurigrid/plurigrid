e7448e876404cc0c2f83e9115f951b934eb6263a
Aja Sibley <aja@thinkum.local>
2023-05-04 13:08:02 -0700

    Added worlding Scheme and Bash scripts for loading and exporting Rewind data.

---
+++ scripts/worlding/bash/reworld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+!#
+pkill Rewind
+
+mv ~/Documents/com.memoryvault.MemoryVault.plist ~/Library/Preferences/com.memoryvault.MemoryVault.plist
+mv ~/Documents/com.memoryvault.MemoryVault ~/Library/Application\ Support/
