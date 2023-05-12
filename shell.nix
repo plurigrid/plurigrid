0a68f4b154a871495b63cf0e0943d7cb0006e20a
Aja Sibley <aja@thinkum.noise>
2023-05-12 03:35:34 +0000

    updated

--- shell.nix
+++ shell.nix
@@ -10,7 +10,23 @@ pkgs.mkShell {
     jq
     stdenv.cc.cc.lib
     libcxx
-  ];
+
+    rustc
+    cargo
+    pkgconfig
+    libiconv
+  ] ++ (lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.OpenGL
+    darwin.apple_sdk.frameworks.CoreServices
+    darwin.apple_sdk.frameworks.AppKit
+    darwin.apple_sdk.frameworks.Foundation
+    darwin.apple_sdk.frameworks.QuartzCore
+    darwin.apple_sdk.frameworks.ApplicationServices
+    darwin.apple_sdk.frameworks.CoreGraphics
+    darwin.apple_sdk.frameworks.CoreVideo
+    darwin.apple_sdk.frameworks.CoreFoundation
+  ]);
+
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]};
     echo "gm gm"
